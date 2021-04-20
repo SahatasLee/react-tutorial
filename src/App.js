442f0c831c09f3ea3bd0f4c3be71440c4710216d
SahatasLee <Sahataslee@outlook.co.th>
2021-04-20 13:07:20 +0700

    onclick

--- src/App.js
+++ src/App.js
@@ -13,7 +13,26 @@ function App() {
 
   const [data, setdata] = useState(0);
 
+  // useEffect(() => {
+  //   if (data > 10 || data > -10 ){ 
+  //     setdata(0)
+  //   }
+  // }, [])
 
+  // function checkData () {
+  //   if (data > 9 || data < -10 ){ 
+  //         setdata(0)
+  //       }
+  // }
+
+  function onhandleClick (action) {
+    if (action > 9 || action < -10 ){ 
+      setdata(0)
+    }
+    else {
+      setdata(action)
+    }
+  }
 
   return (
     <div>
@@ -24,9 +43,9 @@ function App() {
       <div class="card witj position-relative">
         <div class="card-body position-relative ">
           <h5 class="card-title" style={{textAlign: 'center'}}>Score : {data}</h5>
-          <button type="button" class="btn btn-outline-primary cardm" onClick={() => setdata(data + 1)}>+1</button>
-          <button type="button" class="btn btn-outline-danger cardm" onClick={() => setdata(data - 1)}>-1</button>
-          <button type="button" class="btn btn-outline-warning cardm" onClick={() => setdata(0)}>0</button>
+          <button type="button" class="btn btn-outline-primary cardm" onClick={() => onhandleClick(data + 1)}>+1</button>
+          <button type="button" class="btn btn-outline-danger cardm" onClick={() => onhandleClick(data - 1)}>-1</button>
+          <button type="button" class="btn btn-outline-warning cardm" onClick={() => onhandleClick(0)}>0</button>
         </div>
       </div>
     </div>
