54420e8413207e561dd780e44ae4cd63a47ac4e2
SahatasLee <Sahataslee@outlook.co.th>
2021-04-20 10:19:20 +0700

    bootstap5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import Header from './component/Header';
 
 function App() {
+
+  const bootstrap = (
+    <div class="alert alert-primary" role="alert">
+      Welcome to React World
+    </div>
+  )
+
+  const [data, setdata] = useState(0);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header title="Daily Code" />
+      <div className="App">
+        {bootstrap}
+      </div>
+      <div class="card witj position-relative">
+        <div class="card-body position-relative ">
+          <h5 class="card-title" style={{textAlign: 'center'}}>Score : {data}</h5>
+          <button type="button" class="btn btn-outline-primary cardm" onClick={() => setdata(data + 1)}>+1</button>
+          <button type="button" class="btn btn-outline-danger cardm" onClick={() => setdata(data - 1)}>-1</button>
+          <button type="button" class="btn btn-outline-warning cardm" onClick={() => setdata(0)}>0</button>
+        </div>
+      </div>
     </div>
   );
 }
